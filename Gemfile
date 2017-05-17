17b91bff0a4de92d57a387dc8d7c3c3209dd2364
Jonathan Hoyt <hoyt@github.com>
2017-05-17 00:03:31 -0700

    Log request ip and timestamp

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "https://rubygems.org"
 
-gem "sinatra"
 gem "commonmarker"
+gem "pg"
+gem "sinatra"
--- reqip.rb
+++ reqip.rb
@@ -1,10 +1,40 @@
-require "sinatra"
 require "commonmarker"
+require "pg"
+require "sinatra"
+
+db = PG::Connection.new(ENV["DATABASE_URL"] || "postgres://localhost/reqip")
+
+begin
+  db.exec("SELECT id FROM requests LIMIT 1")
+rescue
+  db.exec <<-SQL
+    CREATE TABLE requests (
+      id bigserial PRIMARY KEY,
+      created_at timestamp without time zone,
+      ip inet
+    );
+  SQL
+end
 
 index_template = File.read("./index.template")
 readme_markdown = File.read("./README.md")
-readme_html = CommonMarker.render_html(readme_markdown)
 
 get "/" do
-  index_template.gsub(/YIELD_HTML_HERE/, readme_html)
+  timestamp = Time.now.utc.round(10).iso8601(6)
+
+  begin
+    db.exec <<-SQL
+      INSERT INTO requests (created_at, ip)
+      VALUES (timestamp '#{timestamp}', '#{request.ip}')
+    SQL
+  rescue
+  end
+
+  markdown = [
+    readme_markdown,
+    "* **Your ip address is:** #{request.ip}",
+    "* **The time is:** #{timestamp} UTC"
+  ].join("\n")
+
+  index_template.gsub(/YIELD_HTML_HERE/, CommonMarker.render_html(markdown))
 end
