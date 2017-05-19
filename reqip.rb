54a111c70d514a64cd98176aaa3c68da23891b6f
Jonathan Hoyt <hoyt@github.com>
2017-05-18 21:13:28 -0700

    Simplify requests schema

--- reqip.rb
+++ reqip.rb
@@ -5,35 +5,22 @@ require "sinatra"
 db = PG::Connection.new(ENV["DATABASE_URL"] || "postgres://localhost/reqip")
 
 begin
-  db.exec("SELECT id FROM requests LIMIT 1")
+  db.exec("SELECT * FROM requests LIMIT 1")
 rescue
-  db.exec <<-SQL
-    CREATE TABLE requests (
-      id bigserial PRIMARY KEY,
-      created_at timestamp without time zone,
-      ip inet
-    );
-  SQL
+  db.exec("CREATE TABLE requests (ip inet)")
+  db.exec("CREATE UNIQUE INDEX index_on_ip ON requests (ip)")
 end
 
 index_template = File.read("./index.template")
 readme_markdown = File.read("./README.md")
 
 get "/" do
-  timestamp = Time.now.utc.round(10).iso8601(6)
-
-  begin
-    db.exec <<-SQL
-      INSERT INTO requests (created_at, ip)
-      VALUES (timestamp '#{timestamp}', '#{request.ip}')
-    SQL
-  rescue
-  end
+  db.exec("INSERT INTO requests (ip) VALUES ('#{request.ip}') ON CONFLICT DO NOTHING")
 
   markdown = [
     readme_markdown,
     "* **Your ip address is:** #{request.ip}",
-    "* **The time is:** #{timestamp} UTC"
+    "* **The time is:** #{Time.now.utc.round(10).iso8601(6)} UTC"
   ].join("\n")
 
   index_template.gsub(/YIELD_HTML_HERE/, CommonMarker.render_html(markdown))
